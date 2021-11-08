c7237c31ce16cbe614d18cff3465c4e42e4521d8
Richard Hagen <richard.hagen@gmail.com>
2021-11-08 22:12:39 +0100

    added client apis

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,12 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 /* tslint:disable */
 /* eslint-disable */
-__export(require("./rpc/src/runtime"));
-__export(require("./rpc/src/apis"));
-__export(require("./rpc/src/models"));
+__export(require("./autopilot/src/apis/AutopilotApi"));
+__export(require("./invoices/src/apis/InvoicesApi"));
+__export(require("./rpc/src/apis/LightningApi"));
+// export * from "./signer/src/apis/SignerApi"
+__export(require("./stateservice/src/apis/StateApi"));
+__export(require("./verrpc/src/apis/VersionerApi"));
+// export * from "./walletkit/src/apis/WalletKitApi"
+__export(require("./walletunlocker/src/apis/WalletUnlockerApi"));
+__export(require("./watchtower/src/apis/WatchtowerApi"));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './rpc/src/runtime';
-export * from './rpc/src/apis';
-export * from './rpc/src/models';
+export * from "./autopilot/src/apis/AutopilotApi"
+export * from "./invoices/src/apis/InvoicesApi"
+export * from "./rpc/src/apis/LightningApi"
+// export * from "./signer/src/apis/SignerApi"
+export * from "./stateservice/src/apis/StateApi"
+export * from "./verrpc/src/apis/VersionerApi"
+// export * from "./walletkit/src/apis/WalletKitApi"
+export * from "./walletunlocker/src/apis/WalletUnlockerApi"
+export * from "./watchtower/src/apis/WatchtowerApi"
